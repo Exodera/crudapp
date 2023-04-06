9c0515c666acc4413bb27062297db1aa5e02ab65
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2023-04-06 21:19:15 +0200

    done: basic server config

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  const bodyparser = require('body-parser');
  const morgan = require('morgan');
  const dotenv = require('dotenv');
+ const path = require('path');
  
  const app = express();
  
@@ -14,6 +15,16 @@
  //log requests to body-parser
  app.use(bodyparser.urlencoded({extended: true}));
 
+ //Set view engine
+ app.set('view engine','ejs');
+ 
+//load assets
+app.use('/css',express.static(path.resolve(__dirname,'assets/css')));
+app.use('/js',express.static(path.resolve(__dirname,'assets/js')));
+app.use('/img',express.static(path.resolve(__dirname,'assets/img')));
+
+
+//handle requests?
  app.get('/', (req,res)=>{
     res.send("/ of crud app")
  });
